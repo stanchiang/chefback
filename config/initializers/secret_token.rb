296165368dcabb7a02d918e996a2e0c501e97390
Jon Cheng <jon.syndicate@gmail.com>
2013-06-01 16:28:29 -0400

    removed everything. starting clean

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Chefback::Application.config.secret_token = '2915b13ae5beb95b402a4c3ef48ff425c29d9122e9b9a002d349d5a354e1956bf4f39a3816396affc702fa33042d541a1ed07a111ce0b0b88c71978458eac6ac'
