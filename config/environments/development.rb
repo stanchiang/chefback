dd427fe8c6178070277becd535b069bda9c774b8
Stanley Chiang <stanchiang@outlook.com>
2013-06-01 15:57:48 -0400

    migrations

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-Chefback::Application.configure do
+Angelhack::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # In the development environment your application's code is reloaded on
--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Chefback::Application.config.secret_token = '2915b13ae5beb95b402a4c3ef48ff425c29d9122e9b9a002d349d5a354e1956bf4f39a3816396affc702fa33042d541a1ed07a111ce0b0b88c71978458eac6ac'
+Angelhack::Application.config.secret_token = '42549f54198e2c6c11005723f57b1011bddc5ea6e5668d254c598e7b85a74138ace09c5085252afd75752790fbf984033e94625567b96180142733131b3bc44e'
