304e3aa68fa957b73a6c149c72464ea650737170
Jon Cheng <jon.syndicate@gmail.com>
2013-06-01 16:38:35 -0400

    clean rails app install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chefback::Application.config.secret_token = '6f4ff8514c008501eae61fe50b5c91ad02b81af9bda2597b4c68161321320ac8448fae7cdc553354afb374b3840532434f76048f3532e9ff67d85b5fc5f73c9b'
